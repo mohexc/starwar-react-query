511a29c41f9420d54fd11b895342c3493402371f
nanut <galunyu@gmail.com>
2020-10-24 01:24:18 +0700

    [Add] Navbar, Peple, Planets

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Button } from 'antd'
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Button>Planets</Button>
+            <Button>People</Button>
+        </nav>
+    )
+}
+
+export default Navbar
