a3fa2fcb094c1495eb470fac6b1f1f9cf09e6fa2
nanut <galunyu@gmail.com>
2020-10-24 13:50:51 +0700

    [Added] usePaginatedQuery

--- src/components/People.js
+++ src/components/People.js
@@ -1,24 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./People.less"
-import { useQuery } from "react-query"
-import { Skeleton } from 'antd'
+import { usePaginatedQuery } from "react-query"
+import { Button, Skeleton } from 'antd'
 import Person from "./Person"
 
-const fetchPeople = async () => {
-    const res = await fetch('https://swapi.dev/api/people/')
+const fetchPeople = async (key, page) => {
+    const res = await fetch(`https://swapi.dev/api/people/?page=${page}`)
     return res.json()
 }
 
 const People = () => {
-    const { data, status } = useQuery("people", fetchPeople)
-    console.log(data)
+    const [page, setPage] = useState(1)
+    const { resolvedData, latestData, status } = usePaginatedQuery(["people", page], fetchPeople)
+
+    console.log(resolvedData, `resolveData`)
+    debugger
     return (
         <div className="people">
             <h2>People</h2>
+            <Button disabled={page === 1} className="btn-lift" type="primary" onClick={() => setPage(old => Math.max(old - 1, 1))}>Previous page</Button>
+            <span style={{ marginLeft: "1rem", marginRight: "1rem" }}>{page}</span>
+            <Button disabled={!latestData || !latestData.next} className="btn-lift" type="primary" onClick={() => setPage(old => (!latestData || !latestData.next ? old : old + 1))}>Next page</Button>
             {status === "error" && <div>Error fetching data</div>}
             {status === "loading" && <Skeleton active />}
             {status === "success" && (
-                data.results.map(person => <Person key={person.created} person={person} />)
+                resolvedData.results.map(person => <Person key={person.created} person={person} />)
             )}
         </div>
     )
