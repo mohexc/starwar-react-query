57a606d2feea25556417a83584045483e537fdf3
nanut <galunyu@gmail.com>
2020-10-24 10:51:23 +0700

    [Added] Planet component

---
+++ src/components/Planet.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Planet.less"
+
+const Planet = ({ planet }) => {
+    return (
+        <div className="planet">
+            <h3>{planet.name}</h3>
+            <p>Population - {planet.population}</p>
+            <p>Terrain - {planet.terrain}</p>
+        </div>
+    )
+}
+
+export default Planet
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import "./Planets.less"
 import { useQuery } from "react-query"
+import Planet from './Planet'
 
 const fetchPlanets = async () => {
-    const res = await fetch('http://sswapi.dev/api/planets/')
+    const res = await fetch('http://swapi.dev/api/planets/')
     return res.json()
 }
 
@@ -12,7 +13,11 @@ const Planets = () => {
     console.log(data)
     return (
         <div className="Planets">
-            {status}
+            {status === "error" && <div>Error fetching data</div>}
+            {status === "loading" && <div>Loading data...</div>}
+            {status === "success" && (
+                data.results.map(planet => <Planet key={planet.created} planet={planet} />)
+            )}
         </div>
     )
 }
